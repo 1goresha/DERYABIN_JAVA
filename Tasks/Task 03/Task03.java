d147dc26465f5ae125100f7b8dd196c5245727b6
Deryabin igor <i.deryabin2017@mail.ru>
2018-12-18 20:41:40 +0300

    add home work (1-st lesson)

---
+++ Tasks/Task 03/Task03.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+class Task03 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int a = random.nextInt(255);
+		int b;
+
+		b = a % 2;
+		System.out.println(b+"   A");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"  / \\");	
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+	}
+}
