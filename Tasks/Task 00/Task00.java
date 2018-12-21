4118e84b2a84dc2ddf1a1bf9b48031849f002dcb
Deryabin igor <i.deryabin2017@mail.ru>
2018-12-21 10:35:10 +0300

    rename folders

---
+++ Tasks/Task 00/Task00.java
@@ -0,0 +1,5 @@
+class Task00{
+	public static void main(String[] args) {
+		System.out.println("2");
+	}
+}
--- Tasks/Task 02/Task02.java
+++ Tasks/Task 02/Task02.java
@@ -1,31 +1,43 @@
-class Task02{
+import java.util.Random;
+
+class Task02 {
 	public static void main(String[] args) {
-		int currentSum = 15687;// текущая сумма
+		Random random = new Random();
+		int a = random.nextInt(255);
+		int b;
+
+		b = a % 2;
+		System.out.println(b+"   A");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"  / \\");	
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
+		b = a % 2;
+		System.out.println(b+"   |");
+		a = a / 2;
+
 		
-		
-		System.out.println("5000 - " + currentSum / 5000);
-		currentSum = currentSum % 5000;
-
-		System.out.println("2000 - " +  currentSum / 2000);
-		currentSum = currentSum % 2000;
-
-		System.out.println("1000 - " +  currentSum / 1000);
-		currentSum = currentSum % 1000;
-
-		System.out.println("500 - " +  currentSum / 500);
-		currentSum = currentSum % 500;
-
-		System.out.println("200 - " +  currentSum / 200);
-		currentSum = currentSum % 200;
-
-		System.out.println("100 - " +  currentSum / 100);
-		currentSum = currentSum % 100;
-
-		System.out.println("50 - " +  currentSum / 50);
-		currentSum = currentSum % 50;
-
-		System.out.println("10 - " +  currentSum / 10);
-		currentSum = currentSum % 10;			
 	}
-	
 }
