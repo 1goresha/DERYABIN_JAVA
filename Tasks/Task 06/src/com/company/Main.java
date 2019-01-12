c3572a100b28c603171aa222c7643d63b41ff1bf
Deryabin igor <i.deryabin2017@mail.ru>
2019-01-12 11:58:09 +0300

    use function and methods

--- Tasks/Task 06/src/com/company/Main.java
+++ Tasks/Task 06/src/com/company/Main.java
@@ -1,11 +1,32 @@
 package com.company;
 
 public class Main {
+    public static void reverseArray(int[] array){
+        int temp;
+        for(int i = 0; i<array.length/2; i++){
+            temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+        for (int i=0; i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
 
     public static void main(String[] args) {
 	// write your code here
-        int array[]={3,4,5,6,7};
-        for(int j=array.length-1;j>=0;j--) System.out.println(array[j]);
 
+        /*int temp;
+        int array[]={3,4,5,6,7};
+        for(int i = 0; i<array.length/2; i++){
+            temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+        for (int i=0; i<array.length;i++){
+            System.out.println(array[i]);
+        }*/
+        int[] array = {0,1,2,3,4,5,6,7,8,9};
+        reverseArray(array);
     }
 }
