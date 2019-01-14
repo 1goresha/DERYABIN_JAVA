7f94a0a88c4b9eb73b568dfb0a39c5396efe563a
Deryabin igor <i.deryabin2017@mail.ru>
2019-01-14 12:00:40 +0300

    home work

--- Tasks/Task 07/src/com/company/Main.java
+++ Tasks/Task 07/src/com/company/Main.java
@@ -2,8 +2,28 @@ package com.company;
 
 public class Main {
 
+    public static int quantityOfLocalNum(int []arr){
+        int countLocal = 0;
+        int localMin = 0;
+        int localMax = 0;
+        for(int i =0; i < arr.length; i++){
+            if(i == 0){
+                i++;
+            }
+            if(i == arr.length-1){
+                return countLocal;
+            }
+            localMin++;
+            localMax++;
+            countLocal = localMin + localMax;
+        }
+        return countLocal ;
+    }
+
     public static void main(String[] args) {
 	// write your code here
+    int []arr = {0,1,2,3,4};
+        System.out.println(quantityOfLocalNum(arr));
 
     }
 }
